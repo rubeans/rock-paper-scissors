ab1ffa09ac2c5e068ba23503fe6cf2a7c98ec270
rubeans <rubensglf@gmail.com>
2022-08-29 21:08:57 -0300

    change the index.html page to only one player instead of two and add a function getComputerChoice on the js file

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Hello World.")
+const getComputerChoice = () => {
+    let letter = ["Rock", "Paper", "Scissors"]
+    let choosenLetter = letter[Math.floor(Math.random() * letter.length)]
+    console.log(choosenLetter)
+}
