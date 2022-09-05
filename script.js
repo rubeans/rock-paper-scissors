2e5b766a2243c1087d94824f020b6a1712118a22
rubeans <rubensglf@gmail.com>
2022-09-04 23:45:58 -0300

    separete the challenge code into a new file and handle the click on each button

--- script.js
+++ script.js
@@ -1,51 +1,10 @@
-// WHAT TOP REQUESTED FOR ME TO DO
-// const getComputerChoice = () => {
-//     let letter = ["Rock", "Paper", "Scissors"]
-//     let choosenLetter = letter[Math.floor(Math.random() * letter.length)]
-//     return choosenLetter
-// }
-
-// const playRound = (playerSelection, computerSelection) => {
-//     // player
-//     let firstLttrPlayer = playerSelection.slice(0, 1).toUpperCase()
-//     let restOfTheLttrPlayer = playerSelection.slice(1, playerSelection.length).toLowerCase()
-//     let playerChoice = firstLttrPlayer + restOfTheLttrPlayer
-
-//     // computer
-//     let firstLttrComputer = computerSelection.slice(0, 1).toUpperCase()
-//     let restOfTheLttrComputer = computerSelection.slice(1, computerSelection.length).toLowerCase()
-//     let computerChoice = firstLttrComputer + restOfTheLttrComputer
-
-//     // logic
-//     if (playerChoice === computerChoice && playerChoice === "Rock" && computerChoice === "Rock" || playerChoice === "Paper" && computerChoice === "Paper" || playerChoice === "Scissors" && computerChoice === "Scissors") {
-//         return "Draw!"
-//     }
-//     else if (playerChoice == "Rock" && computerChoice == "Scissors" || playerChoice == "Paper" && computerChoice == "Rock" || playerChoice == "Scissors" && computerChoice == "Paper") {
-//         return "You win! " + playerChoice + " beats " + computerChoice + "."
-//     }
-//     else if (computerChoice === "Rock" && playerChoice === "Scissors" || computerChoice === "Paper" && playerChoice === "Rock" || computerChoice === "Scissors" && playerChoice === "Paper") {
-//         return "You lose! " + computerChoice + " beats " + playerChoice + "."
-//     }
-//     else {
-//         return "This isn't an option. Please, choose between Rock, Paper or Scissors"
-//     }
-// }
-
-// const game = () => {
-//     const getPlayerChoice = prompt("Please, choose between Rock, Paper or Scissors.")
-//     alert(playRound(getPlayerChoice, getComputerChoice()))
-// }
-
-// game()
-
-
-
-// PERSONAL - THIS IS WHAT I WILL USE FOR THIS PROJECT
-
 const rock = document.querySelector(".rock")
 const paper = document.querySelector(".paper")
 const scissors = document.querySelector(".scissors")
+const allButtons = document.querySelectorAll(".btn")
 
-let playerPoints = 0
-let computerPoints = 0
-let message = ""
+for(let i = 0; i < allButtons.length; i++){
+    allButtons[i].addEventListener("click", () =>{
+        console.log("click")
+    })
+}
