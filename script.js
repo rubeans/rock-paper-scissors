dcf45df51ef7fd0f2638ef89214d83c1d5b06c9e
rubeans <rubensglf@gmail.com>
2022-09-07 21:10:09 -0300

    keep the old script file but with small changes ann reajust the html and css files as well

--- script.js
+++ script.js
@@ -1,10 +1,78 @@
-const rock = document.querySelector(".rock")
-const paper = document.querySelector(".paper")
-const scissors = document.querySelector(".scissors")
-const allButtons = document.querySelectorAll(".btn")
+// WHAT TOP REQUESTED FOR ME TO DO
 
-for(let i = 0; i < allButtons.length; i++){
-    allButtons[i].addEventListener("click", () =>{
-        console.log("click")
-    })
+// GET THE ELEMENTS
+let heading = document.querySelector("h1");
+let paragraph = document.querySelector("p");
+
+// INITIAL COUNT
+let playerCount = 0
+let computerCount = 0
+
+// GET COMPUTER CHOICE
+const getComputerChoice = () => {
+    let letter = ["Rock", "Paper", "Scissors"]
+    let choosenLetter = letter[Math.floor(Math.random() * letter.length)]
+    return choosenLetter
+}
+
+// ROUNDS
+const playRound = (playerSelection, computerSelection) => {
+    // PLAYER CHOICE
+    let firstLttrPlayer = playerSelection.slice(0, 1).toUpperCase()
+    let restOfTheLttrPlayer = playerSelection.slice(1, playerSelection.length).toLowerCase()
+    let playerChoice = firstLttrPlayer + restOfTheLttrPlayer
+
+    // COMPUTER CHOICE
+    let firstLttrComputer = computerSelection.slice(0, 1).toUpperCase()
+    let restOfTheLttrComputer = computerSelection.slice(1, computerSelection.length).toLowerCase()
+    let computerChoice = firstLttrComputer + restOfTheLttrComputer
+
+    // LOGIC
+    if (playerChoice === computerChoice && playerChoice === "Rock" && computerChoice === "Rock" || playerChoice === "Paper" && computerChoice === "Paper" || playerChoice === "Scissors" && computerChoice === "Scissors") {
+        playerCount += 0
+        computerCount += 0
+        return "Draw!"
+    }
+    else if (playerChoice == "Rock" && computerChoice == "Scissors" || playerChoice == "Paper" && computerChoice == "Rock" || playerChoice == "Scissors" && computerChoice == "Paper") {
+        playerCount += 1
+        computerCount += 0
+        return "Nice choice, you win the round! " + playerChoice + " beats " + computerChoice + "."
+    }
+    else if (computerChoice === "Rock" && playerChoice === "Scissors" || computerChoice === "Paper" && playerChoice === "Rock" || computerChoice === "Scissors" && playerChoice === "Paper") {
+        playerCount += 0
+        computerCount += 1
+        return "Bad choice, computer wins the round! " + computerChoice + " beats " + playerChoice + "."
+    }
+    else {
+        return "This isn't an option. Please, choose between Rock, Paper or Scissors."
+    }
+}
+
+// GAME CONTENT
+const game = () => {
+    //GET PLAYER CHOICE
+    const getPlayerChoice = prompt("Please, choose between Rock, Paper or Scissors.")
+
+    // DISPLAY THE QUESTION
+    alert(playRound(getPlayerChoice, getComputerChoice()))
+
+    // SHOW THE RESULT
+    paragraph.innerText = "Score: " + playerCount + " - " + computerCount
+
+    // SHOW WHO WINS
+    if (playerCount > computerCount) {
+        heading.innerText = "Congrats! You won the game."
+    }
+    else if (playerCount === computerCount) {
+        heading.innerText = "What a tough game. It's a draw!"
+    }
+    else {
+        heading.innerText = "Nice try! The computer was smarter this time."
+    }
+}
+
+const startGame = () => {
+    for (let i = 0; i < 5; i++) {
+        game()
+    }
 }
